49d3f82e86416706bc2b9223452fa9eb2a4633b2
Yuki Yamamoto <yuki.yamamoto0915@gmail.com>
2021-05-10 01:35:30 +0900

    axiosの導入

---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="login-wrapper">
+    <form action="login-form" >
+      <p>
+        <label for="name">User:</label>
+        <input v-model="mail">
+      </p>
+      <p>
+        <label for="name">Password:</label>
+        <input v-model="password">
+      </p>
+      <p>
+        <input @click="Login" type="submit" value="submit">
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      mail: '',
+      password: ''
+    }
+  },
+  methods: {
+    Login: function(){
+      axios.post('http://localhost:80/signup', {
+        mail:     this.mail,
+        password: this.password
+      })
+      .then(function(response){
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+    }
+  }
+}
+</script>
