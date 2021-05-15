e29eaee8954f11421b88f98a4aed867659cb70ca
Yuki Yamamoto <yuki.yamamoto0915@gmail.com>
2021-05-15 20:36:06 +0900

    bootstrap 導入

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueCookies from 'vue-cookies'
 import router from './router'
 import VueRouter from 'vue-router'
 
+// VueCookies
+import VueCookies from 'vue-cookies'
+Vue.use(VueCookies)
+
+// Bootstrap
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
 Vue.config.productionTip = false
 
-Vue.use(VueCookies)
 Vue.use(VueRouter)
 
 
